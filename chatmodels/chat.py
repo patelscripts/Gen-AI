fc1743d247ec14ec89366703579ed71b77efb9e8
patelscripts <navneetpatel325@gmail.com>
2026-07-27 23:01:05 +0530

    change the gemini model because of quota limit

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain.chat_models import init_chat_model
 
-init_chat_model()
-
 model = init_chat_model(
-    "gemini-3.5-flash",
+    "gemini-2.5-flash",
     model_provider="google_genai"
 )
 
-print(model)
+response = model.invoke("what is apple ")
+print(response.content)
