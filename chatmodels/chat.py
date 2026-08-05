92f18586040a6c1d903a7c7a82cd7f1aaa15ed3b
patelscripts <navneetpatel325@gmail.com>
2026-08-05 23:23:41 +0530

    i used groq api in place of gemini and gave me a better result

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -1,38 +1,13 @@
-import os
-
 from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
-from langchain_groq import ChatGroq
-
 load_dotenv()
-llm = ChatGroq(
-    model="qwen/qwen3-32b",
-    temperature=0,
-    max_tokens=None,
-    reasoning_format="parsed",
-    timeout=None,
-    max_retries=2,
-)
+from langchain.chat_models import init_chat_model
 
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that translates English to French. Translate the user sentence.",
-    ),
-    ("human", "I love programming."),
-]
-ai_msg = llm.invoke(messages)
+# model = init_chat_model("gemini-2.5-flash",model_provider="google_genai")
 
-print(ai_msg.content)
-
-# if not os.getenv("GOOGLE_API_KEY"):
-#     raise RuntimeError("GOOGLE_API_KEY missing — check your .env file")
-
-# model = init_chat_model(
-#     "gemini-2.5-flash",
-#     model_provider="google_genai",
-# )
-
-# question = "what is football?"
-# response = model.invoke(question)
+# response = model.invoke("Who is virat kohli and why he is famous")
 # print(response.content)
+
+model = init_chat_model("llama-3.3-70b-versatile", model_provider="groq")
+
+answer = model.invoke("do you know who is shahrukh khan?")
+print(answer.content)
